8fbe2fe62ce74bde8d5cc3699b9be7427233f7b7
liangl <liangl@jumei.com>
2019-03-18 15:46:53 +0800

    求最长子串长度

---
+++ src/main/java/algorithm/LongestSubstring.java
@@ -0,0 +1,35 @@
+package algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * Created by liangl on 2019/3/18.
+ */
+public class LongestSubstring {
+    public static void main(String[] args) {
+        String str = "abcabcdefg";
+        System.out.println(lengthOfLongestSubstring(str));
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || "".equals(s)) return 0;
+        Map<String,Integer> map = new HashMap<>();
+        int maxLength = 0;
+        int temp = 0;
+        for (int i = 0;i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.get(String.valueOf(c)) == null) {
+                temp++;
+                map.put(String.valueOf(c),i);
+            } else {
+                i = map.get(String.valueOf(c));
+                maxLength = maxLength >= temp ? maxLength : temp;
+                temp = 0;
+                map.clear();
+            }
+        }
+        maxLength = maxLength >= temp ? maxLength : temp;
+        return maxLength;
+    }
+}
