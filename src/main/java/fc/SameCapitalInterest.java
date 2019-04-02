aab41265bc6d8c5099235e2b7d0f8d43758761c6
liangl <liangl@jumei.com>
2019-04-02 14:10:26 +0800

    等额本金计算

--- src/main/java/fc/SameCapitalInterest.java
+++ src/main/java/fc/SameCapitalInterest.java
@@ -10,9 +10,15 @@ public class SameCapitalInterest {
 
     public static void main(String[] args) {
 
-        calculate(new BigDecimal(10000),new BigDecimal(0.18),12);
+        calculate(new BigDecimal(320000),new BigDecimal(0.05635),30*12);
     }
 
+    /**
+     *
+     * @param capital 本金
+     * @param rate 年化（10%的年化则传0.1）
+     * @param period 周期（月）
+     */
     public static void calculate(BigDecimal capital, BigDecimal rate, Integer period) {
 
         BigDecimal monthRate = rate.divide(new BigDecimal(12)).setScale(6,BigDecimal.ROUND_UP);
@@ -20,7 +26,7 @@ public class SameCapitalInterest {
         BigDecimal var2 = monthRate.add(BigDecimal.ONE).pow(period).subtract(BigDecimal.ONE).setScale(6,BigDecimal.ROUND_UP);
         System.out.println(monthRate+","+var1+","+var2);
         BigDecimal repaymentAmt = monthRate.multiply(var1).divide(var2, 6, BigDecimal.ROUND_UP).multiply(capital).setScale(2,BigDecimal.ROUND_UP);
-        System.out.println(repaymentAmt+",total:"+repaymentAmt.multiply(new BigDecimal(12)));
+        System.out.println("每期："+repaymentAmt);
         BigDecimal remainCapital = capital;
         for (int i = 1;i <= period; i++) {
             BigDecimal var3 = remainCapital.multiply(monthRate);
@@ -30,4 +36,8 @@ public class SameCapitalInterest {
         }
 
     }
+
+    /**
+     * 公积金40W30年1740.83
+     */
 }
