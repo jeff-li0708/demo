7c7892de067d127be26a2bc6f0d1e259491b7ea9
liangl <liangl@jumei.com>
2020-11-13 10:39:40 +0800

    链表

--- src/main/java/algorithm/LinkedListSummary.java
+++ src/main/java/algorithm/LinkedListSummary.java
@@ -374,6 +374,30 @@ public class LinkedListSummary {
         if (head.value == top.value) top = top.next;
     }
 
+    /**
+     * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+     * 示例 1:
+     * 输入: 1->2->3->4->5->NULL
+     * 输出: 1->3->5->2->4->NULL
+     * @param head
+     * @return
+     */
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode evenHead = head.next;
+        ListNode odd = head, even = evenHead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;//奇数的下一个节点是偶数节点的下一个
+            odd = odd.next;//移动奇数节点指针
+            even.next = odd.next;//偶数节点的下一个是奇数节点的下一个
+            even = even.next;//一定偶数节点的指针
+        }
+        odd.next = evenHead;//奇数节点的尾部指向偶数节点的头部
+        return head;
+    }
+
     public static void main(String[] args) {
 
         Scanner in=new Scanner(System.in);
