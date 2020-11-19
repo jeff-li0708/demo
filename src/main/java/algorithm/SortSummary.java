bbcc0ea4fd127aaeb21d647a85df4260c0ec0232
liangl <liangl@jumei.com>
2020-11-19 16:15:52 +0800

    排序

--- src/main/java/algorithm/SortSummary.java
+++ src/main/java/algorithm/SortSummary.java
@@ -45,12 +45,61 @@ public class SortSummary {
         quickSort(nums,i+1,end);
     }
 
+    /**
+     * 堆排序
+     * 利用大顶堆（堆顶元素最大），每次将堆顶元素取出
+     * @param nums
+     */
+    public void heapSort(int[] nums){
+        int n = nums.length;
+        //构建大顶堆
+        for (int i=0;i<n;i++){
+            int k = i;
+            int key = nums[i];
+            while (k > 0) {
+                int parent = (k - 1) >>> 1;
+                int e = nums[parent];
+                if (key<=e)
+                    break;
+                nums[k] = e;
+                k = parent;
+            }
+            nums[k] = key;
+        }
+        int end = n-1;
+        //循环删除堆顶元素冒泡到数组后面，并调整堆
+        while (end>0){
+            int key = nums[end];
+            nums[end]=nums[0];//将堆顶元素放数组最后
+            int k=0;
+            int size = end;
+            int half = size >>> 1;        // loop while a non-leaf
+            while (k < half) {
+                int child = (k << 1) + 1; // assume left child is least
+                int c = nums[child];
+                int right = child + 1;
+                if (right < size && c < nums[right])
+                    c = nums[child = right];
+                if (key >= c)
+                    break;
+                nums[k] = c;
+                k = child;
+            }
+            nums[k] = key;
+            end--;
+        }
+
+    }
+
+
+
     public static void main(String[] args) {
         int[] arr = {12,3,56,44,76,32,4,8,88,66};
         for (int a:arr) System.out.print(a+",");
         System.out.println();
 //        arr = bubbling(arr);
-        new SortSummary().quickSort(arr,0,arr.length -1);
+//        new SortSummary().quickSort(arr,0,arr.length -1);
+        new SortSummary().heapSort(arr);
         for (int a:arr) System.out.print(a + ",");
         String s = "30";
 
